f3a7be736f864ff26ae728998164d4b3b4ea3de2
Kyla <kflmorcillos@gmail.com>
2025-02-21 20:47:25 +0800

    RoomType: Implement getPropertyPrice method

--- src/RoomType.java
+++ src/RoomType.java
@@ -1,25 +1,31 @@
 package src;
 
 public enum RoomType {
-    SINGLE(50, 1),
-    STANDARD_DOUBLE(100, 2),
-    STANDARD_TWIN(120, 2),
-    DELUXE_ROOM(200, 2),
-    STUDIO_APARTMENT(300, 2),
-    JUNIOR_SUITE(500, 3),
-    EXECUTIVE_SUITE(1000, 4),
-    PRESIDENTIAL_SUITE(5000, 6);
+    SINGLE(2000, 50, 1),
+    STANDARD_DOUBLE(5000, 100, 2),
+    STANDARD_TWIN(6000, 120, 2),
+    DELUXE_ROOM(12000, 200, 2),
+    STUDIO_APARTMENT(18000, 300, 2),
+    JUNIOR_SUITE(30000, 500, 3),
+    EXECUTIVE_SUITE(70000, 1000, 4),
+    PRESIDENTIAL_SUITE(200000, 5000, 6);
 
-    private final int pricePerNight;
+    private final int propertyPrice;
+    private final int rentPerNight;
     private final int capacity;
 
-    RoomType(int pricePerNight, int capacity) {
-        this.pricePerNight = pricePerNight;
+    RoomType(int propertyPrice, int rentPerNight, int capacity) {
+        this.propertyPrice = propertyPrice;
+        this.rentPerNight = rentPerNight;
         this.capacity = capacity;
     }
 
-    public int getPricePerNight() {
-        return pricePerNight;
+    public int getPropertyPrice() {
+        return propertyPrice;
+    }
+
+    public int getRentPerNight() {
+        return rentPerNight;
     }
 
     public int getCapacity() {
