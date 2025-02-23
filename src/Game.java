53f86d161a89ce46e474666286b955331f91cdd9
Kyla <kflmorcillos@gmail.com>
2025-02-23 15:54:20 +0800

    Game: Display other room options

--- src/Game.java
+++ src/Game.java
@@ -61,11 +61,97 @@ class Game {
         String headerContent = "Buy a New Room" + String.format("\n" + "Balance: $%.2f", Game.this.getBalance());
 
         RoomType singleRoom = RoomType.SINGLE;
+        RoomType standardDoubleRoom = RoomType.STANDARD_DOUBLE;
+        RoomType standardTwinRoom = RoomType.STANDARD_TWIN;
+        RoomType deluxeRoom = RoomType.DELUXE_ROOM;
+        RoomType studioApartment = RoomType.STUDIO_APARTMENT;
+        RoomType juniorSuite = RoomType.JUNIOR_SUITE;
+        RoomType executiveSuite = RoomType.EXECUTIVE_SUITE;
+        RoomType presidentialSuite = RoomType.PRESIDENTIAL_SUITE;
+
+        // Rooms can only be unlocked at certain levels, but try to display all rooms first.
 
         final Action[] choices = {
             new Action("Single Room") {
                 public void call(UserInteraction userInteraction) {
                     deductBalance(singleRoom.getPropertyPrice());
+                    System.out.println("\nYou bought a Single Room.\nNew Balance: $" + getBalance());
+
+                    System.out.print("\nPress Enter to continue...");
+                    userInteraction.scanner.nextLine();
+
+                    getMainChoices(userInteraction);
+                }
+            },
+            new Action("Standard Double Room") {
+                public void call(UserInteraction userInteraction) {
+                    deductBalance(standardDoubleRoom.getPropertyPrice());
+                    System.out.println("\nYou bought a Single Room.\nNew Balance: $" + getBalance());
+
+                    System.out.print("\nPress Enter to continue...");
+                    userInteraction.scanner.nextLine();
+
+                    getMainChoices(userInteraction);
+                }
+            },
+            new Action("Standard Twin Room") {
+                public void call(UserInteraction userInteraction) {
+                    deductBalance(standardTwinRoom.getPropertyPrice());
+                    System.out.println("\nYou bought a Single Room.\nNew Balance: $" + getBalance());
+
+                    System.out.print("\nPress Enter to continue...");
+                    userInteraction.scanner.nextLine();
+
+                    getMainChoices(userInteraction);
+                }
+            },
+            new Action("Deluxe Room") {
+                public void call(UserInteraction userInteraction) {
+                    deductBalance(deluxeRoom.getPropertyPrice());
+                    System.out.println("\nYou bought a Single Room.\nNew Balance: $" + getBalance());
+
+                    System.out.print("\nPress Enter to continue...");
+                    userInteraction.scanner.nextLine();
+
+                    getMainChoices(userInteraction);
+                }
+            },
+            new Action("Studio Apartment") {
+                public void call(UserInteraction userInteraction) {
+                    deductBalance(studioApartment.getPropertyPrice());
+                    System.out.println("\nYou bought a Single Room.\nNew Balance: $" + getBalance());
+
+                    System.out.print("\nPress Enter to continue...");
+                    userInteraction.scanner.nextLine();
+
+                    getMainChoices(userInteraction);
+                }
+            },
+            new Action("Junior Suite") {
+                public void call(UserInteraction userInteraction) {
+                    deductBalance(juniorSuite.getPropertyPrice());
+                    System.out.println("\nYou bought a Single Room.\nNew Balance: $" + getBalance());
+
+                    System.out.print("\nPress Enter to continue...");
+                    userInteraction.scanner.nextLine();
+
+                    getMainChoices(userInteraction);
+                }
+            },
+            new Action("Executive Suite") {
+                public void call(UserInteraction userInteraction) {
+                    deductBalance(executiveSuite.getPropertyPrice());
+                    System.out.println("\nYou bought a Single Room.\nNew Balance: $" + getBalance());
+
+                    System.out.print("\nPress Enter to continue...");
+                    userInteraction.scanner.nextLine();
+
+                    getMainChoices(userInteraction);
+                }
+            },
+            new Action("Presidential Suite") {
+                public void call(UserInteraction userInteraction) {
+                    deductBalance(presidentialSuite.getPropertyPrice());
                     System.out.println("\nYou bought a Single Room.\nNew Balance: $" + getBalance());
 
                     System.out.print("\nPress Enter to continue...");
